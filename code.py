b41ca51d6f554454c8e25d53cb194b5ebc37d8e3
yannilefki <80094663+yannilefki@users.noreply.github.com>
2021-04-16 12:44:28 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,42 +1,44 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import random
 
 #constantes
 PROB_CONT = 0.2
 PROB_DEATH = 0.1
-NB_TURNS_TO_LIFE = 5 #on effectue 5 fois death(), puis on immunise notre cellule
-
+NB_TURNS_TO_LIFE = 5 #une cellule va soit mourir, soit survivre NB_TURNS_TO_LIFE tours et devenir immunisée
 
 matrice = np.array([[0, 1, 1, 1, 0],
                     [1, 0, 1, 0, 0], 
                     [0, 0, 2, 1, 0],
                     [0, 1, 1, 0, 1],
-                    [1, 0, 1, 1, 0]]) 
+                    [1, 0, 1, 1, 0]])
 
 
-# différents états possibles pour yne cellule :
+
+# differents etats possibles pour une cellule
 # 0 - mort
 # 1 - vivant
-# 2 - contaminé
-# 3 - immunisé
+# 2 - contamine
+# 3 - immunise
 
+def creation_monde(x, y):
+    monde = np.random.randint(4, size=(x, y))
+    return monde
 
-monde = np.random.randint(4, size=(6, 8))
+monde = creation_monde(10, 10)
 print(monde)
 print()
 
 def voisinnage(monde, el): #matrice : List[List[int]], el : Tuple[int, int]
-    """Préconditions : x>1 and y>1
+    """Preconditions : x>1 and y>1
     
-    Renvoie une liste d'indices des éléments dans
-    le voisinage de Moore de l'élément passé en paramètre
+    Renvoie une liste d'indices des elements dans
+    le voisinage de Moore de l'element passe en parametre
     """
     res = []
     
     
-    x = np.shape(matrice)[0]
-    y = np.shape(matrice)[1]
+    x = np.shape(monde)[0]
+    y = np.shape(monde)[1]
     
     xEl = el[0]
     yEl = el[1]
@@ -48,7 +50,7 @@ def voisinnage(monde, el): #matrice : List[List[int]], el : Tuple[int, int]
         for i in range(yEl-1, yEl+2):
             res.append((xEl-1, i))
             
-        #à gauche et à droite
+        #a gauche et a droite
         res.append((xEl, yEl-1))
         res.append((xEl, yEl+1))
         
@@ -56,47 +58,47 @@ def voisinnage(monde, el): #matrice : List[List[int]], el : Tuple[int, int]
         for i in range(yEl-1, yEl+2):
             res.append((xEl+1, i))
     
-    #cas 2 (en haut à gauche)
+    #cas 2 (en haut a gauche)
     elif (xEl == 0 and yEl == 0):
-        #à droite
+        #a droite
         res.append((0, 1))
         #en bas
         res.append((1, 0))
-        #en bas à droite
+        #en bas a droite
         res.append((1, 1))
     
-    #cas 3 (en haut à droite)
+    #cas 3 (en haut a droite)
     elif (xEl == 0 and yEl == y-1):
-        #à gauche
+        #a gauche
         res.append((0, y-2))
-        #en bas à gauche
+        #en bas a gauche
         res.append((1, y-2))
         #en bas
         res.append((1, y-1))  
     
-    #cas 4 (en bas à gauche)
+    #cas 4 (en bas a gauche)
     elif (xEl == x-1 and yEl == 0):
         #en haut
         res.append((x-2, 0))
-        #en haut à droite
+        #en haut a droite
         res.append((x-2, 1))
-        #à droite
+        #a droite
         res.append((x-1, 1))
     
-    #cas 5 (en bas à droite)
+    #cas 5 (en bas a droite)
     elif (xEl == x-1 and yEl == y-1):
-        #en haut à gauche
+        #en haut a gauche
         res.append((x-2, y-2))
         #en haut
         res.append((x-2, y-1))
-        #à gauche
+        #a gauche
         res.append((x-1, y-2))
 
     #cas 6 (bord haut)
     elif (xEl == 0):
-        #à gauche
+        #a gauche
         res.append((0, yEl-1))
-        #à droite
+        #a droite
         res.append((0, yEl+1))
         #en bas
         for i in range(yEl-1, yEl+2):
@@ -107,7 +109,7 @@ def voisinnage(monde, el): #matrice : List[List[int]], el : Tuple[int, int]
         #en haut
         res.append((xEl-1, 0))
         res.append((xEl-1, 1))
-        #à droite
+        #a droite
         res.append((xEl, 1))
         #en bas
         res.append((xEl+1, 0))
@@ -118,7 +120,7 @@ def voisinnage(monde, el): #matrice : List[List[int]], el : Tuple[int, int]
         #en haut
         res.append((xEl-1, y-2))
         res.append((xEl-1, y-1))
-        #à gauche
+        #a gauche
         res.append((xEl, y-2))
         #en bas
         res.append((xEl+1, y-2))
@@ -129,33 +131,64 @@ def voisinnage(monde, el): #matrice : List[List[int]], el : Tuple[int, int]
         #en haut
         for i in range(yEl-1, yEl+2):
             res.append((xEl-1, i))
-        #à gauche
+        #a gauche
         res.append((x-1, yEl-1))
-        #à droite
+        #a droite
         res.append((x-1, yEl+1))
         
     return res
 
 def contamination(monde, el, prob_cont):
-    """Préconditions : el appartient à monde et monde[el] == 2; prob_cont >= 0.0 and prob_cont <= 1.0
-    Contamine toutes les cellules en vie autour de notre el avec une proba de pron_cont
+    """Preconditions : el appartient a monde et monde[el] == 2; prob_cont >= 0.0 and prob_cont <= 1.0
+    Renvoie la liste des cellules contaminées par notre cellule el
     """
     
+    liste_cont = []
+    
+    
     for i in voisinnage(monde, el):
-        if (monde[i] == 1 and random.random()<=prob_cont):
-            monde[i] = 2
+        if random.random()<=prob_cont:
+            liste_cont.append(voisinnage[i])
 
-    return monde
+    return liste_cont
 
 def death(monde, el, prob_death):
-    """Préconditions : el appartient à monde et monde[el] == 2; prob_death >= 0.0 and prob_death <= 1.0
-    Tue une cellule infectée avec une probabilité de prob_death"""
+    """Preconditions : el appartient a monde et monde[el] == 2; prob_death >= 0.0 and prob_death <= 1.0
+    Tue une cellule infectee avec une probabilite de prob_death"""
     if random.random()<=prob_death:
-        monde[el] = 0
+        return 1
+    else:
+        return 0
     
-    return monde
+def tour(monde):
+    
+    x = np.shape(monde)[0]
+    y = np.shape(monde)[1]
+    
+    monde_update = np.zeros(x,y)
+    
+    for i in range(x):
+        for j in range(y):
+            if monde[i][j] == 0:
+                monde_update[i][j] == 0
+            elif monde[i][j] == 3:
+                monde_update[i][j] == 3
     
     
+    for i in range(x):
+        for j in range(y):
+            if monde[i][j] == 2:
+                monde_update[i][j] == 2
+                for k in contamination(monde, (i, j), PROB_CONT):
+                    monde_update[k] = 2
+                    
+            
+    
+    return 
+
+
+# liste des contaminés a chaque tour -> pouvoir les immuniser au bout de NB_TURNS_TO_LIFE
+
 #Jeu de tests
 #cas 1
 assert(voisinnage(matrice, (2, 2))) == [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)]
@@ -175,4 +208,3 @@ assert(voisinnage(matrice, (2, 0))) == [(1, 0), (1, 1), (2, 1), (3, 0), (3, 1)]
 assert(voisinnage(matrice, (2, 4))) == [(1, 3), (1, 4), (2, 3), (3, 3), (3, 4)]
 #cas 9
 assert(voisinnage(matrice, (4, 2))) == [(3, 1), (3, 2), (3, 3), (4, 1), (4, 3)]
-
